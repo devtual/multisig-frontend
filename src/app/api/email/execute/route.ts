04d7b5dae29392236d73aaa544e737554c3a7c80
devtual <devtual@gmail.com>
2025-08-06 23:55:24 +0500

    Email changes

--- src/app/api/email/execute/route.ts
+++ src/app/api/email/execute/route.ts
@@ -1,7 +1,7 @@
 import { sendExecutionEmail } from '@/backend/lib/email';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function POST(req:NextApiRequest, res:NextApiResponse) {
+export async function POST(req:NextApiRequest, res:NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
