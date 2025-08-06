2c11b8877d33d9e18722bd63ebb16acd1ceb1350
devtual <devtual@gmail.com>
2025-08-06 23:49:51 +0500

    Email changes

--- src/app/api/email/confirm/route.ts
+++ src/app/api/email/confirm/route.ts
@@ -1,7 +1,7 @@
 import { sendConfirmationEmail } from '@/backend/lib/email';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse) {
+export default async function POST(req:NextApiRequest, res:NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed' });
   }
