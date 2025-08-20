cee0a62cb5aff13edf161a6461581519c34de8b0
devtual <devtual@gmail.com>
2025-08-20 13:46:31 +0500

    Owners

--- src/app/(main)/owners/page.tsx
+++ src/app/(main)/owners/page.tsx
@@ -1,12 +1,16 @@
-import { getOwners } from '@/backend/actions/owners';
+import { getOwners, mergeOwners } from '@/backend/actions/owners';
+import Loader from '@/components/Loader';
 import OwnersList from '@/components/OwnersList';
 import { MultiSigService } from '@/services/multisig-service';
-import ownerService from '@/services/owner-service';
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export default async function OwnersPage() {
   const contract = await MultiSigService.getReadOnlyContract();
   const [onChainOwners, dbOwners] = await Promise.all([contract.getOwners(), getOwners()]);
-  const owners = await ownerService.mergeOwners(onChainOwners, dbOwners);
-  return <OwnersList initialOwners={owners} />;
+
+  const owners = await mergeOwners(onChainOwners, dbOwners);
+  
+  return <Suspense fallback={<Loader variant='fullscreen' />}>
+    <OwnersList initialOwners={owners} />
+  </Suspense>
 }
